1f1b3c52d43272d5119c6dd6d165ddb99a4681ec
edgardjustino <eddiejustino@gmail.com>
2023-06-09 23:40:58 -0300

    adding javascript

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const ratingNumbers = document.querySelectorAll('card__rating-number');
+const ratingNumbersDOM = document.querySelectorAll('.card__rating-number');
+const mainCardDom = document.querySelector('.card--main');
+const thankYouCardDOM = document.querySelector('.card--thank-you');
+const resultValueDOM = document.querySelector('.card__result-value');
 
-function selectRating (number){};
+let activeRatingNumber;
 
-function submit (){};
+function selectRating(number) {
+  const ratingNumbersDOM = document.querySelectorAll('.card__rating-number');
+  const selectedRatingNumberDOM = ratingNumbersDOM[number - 1];
+
+  if (activeRatingNumber) {
+    const activeRatingDom = ratingNumbersDOM[activeRatingNumber - 1];
+    activeRatingDom.classList.remove('card__rating-number-active');
+  }
+  selectedRatingNumberDOM.classList.add('card__rating-number-active');
+  activeRatingNumber = number;
+}
+
+function submit() {
+  if (activeRatingNumber) {
+    mainCardDom.classList.add('hide');
+    thankYouCardDOM.classList.remove('hide');
+    resultValueDOM.textContent = activeRatingNumber;
+  }
+}
